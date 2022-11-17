197a2c18bf9997d803bb6e111dd809b4d4d7d667
Sebastian Vargaz <juancbastianrv@gmail.com>
2022-11-17 11:54:01 -0500

    implementacion por modulos

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-include 
-    <header class="header">
-      <!-- icono de la empresa -->
-      <img
-        class="iconEmpresa"
-        src="https://automarcol.com/wp-content/uploads/2021/12/abc-logo-automarcol@2x-1-1024x230.png"
-        alt=""
-      />
-      <ul class="ulHeader">
+<?php include'./includes/components/header.php';?>
+
+<header class="header">
+    <!-- icono de la empresa -->
+    <img class="iconEmpresa"
+        src="https://automarcol.com/wp-content/uploads/2021/12/abc-logo-automarcol@2x-1-1024x230.png" alt="" />
+    <ul class="ulHeader">
         <li class="liHeader">
-          <a href="#">home</a>
+            <a href="#">home</a>
         </li>
         <li class="liHeader">
-          <a href="#">cars</a>
+            <a href="#">cars</a>
         </li>
         <li class="liHeader">
-          <a href="#">motocards</a>
+            <a href="#">motocards</a>
         </li>
-      </ul>
-    </header>
+    </ul>
+</header>
+
+<?php include'./includes/components/footer.php';?>
